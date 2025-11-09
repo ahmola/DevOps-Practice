599bc77582b72c2585d2bd8e746cbf04c61a08f4
Jason <66013853+ahmola@users.noreply.github.com>
2025-11-09 16:54:52 +0900

    prometheus

--- docker/run-compose.sh
+++ docker/run-compose.sh
@@ -1,4 +1,7 @@
 sh build-container.sh
 
 docker compose -p crud-gateway-consul -f docker-compose-crud.dev.yml down -v --remove-orphans
+docker compose -p monitoring -f docker-compose-monitor.dev.yml down -v --remove-orphans
+
+docker compose -p monitoring -f docker-compose-monitor.dev.yml up --build -d
 docker compose -p crud-gateway-consul -f docker-compose-crud.dev.yml up --build -d
